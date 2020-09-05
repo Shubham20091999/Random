da7e176d08c89c52ae992c0f47fc80bc9a8e27ce
Shubham20091999 <shubham20091999@gmail.com>
2020-09-05 13:46:57 +0530

    Binary Tree BFT DFT

--- Data Structures/BinaryTree.py
+++ Data Structures/BinaryTree.py
@@ -1,12 +1,10 @@
 from requests.api import head
 
-
 class Node:
     def __init__(self, v, l=None, r=None):
         self.val = v
         self.left = l
         self.right = r
-
 
 class BinaryTree:
     def __init__(self, a: list = []):
@@ -14,10 +12,31 @@ class BinaryTree:
             self.head = None
         else:
             self.head = Node(a[0])
-            for e in a[1:]:
-                self.insert(e)
+            BinaryTree.__helper(a[1:], [self.head])
+
+    @staticmethod
+    def __helper(arr, prevNodeArr):
+        if(len(arr) == 0):
+            return
+        currActiveNodes = []
+        for p in prevNodeArr:
+            if(len(arr) == 0):
+                break
+            if(arr[0] != None):
+                p.left = Node(arr[0])
+                currActiveNodes.append(p.left)
+            arr.pop(0)
+            if(len(arr) == 0):
+                break
+            if(arr[0] != None):
+                p.right = Node(arr[0])
+                currActiveNodes.append(p.right)
+            arr.pop(0)
+        else:
+            BinaryTree.__helper(arr, currActiveNodes)
 
     # https://www.geeksforgeeks.org/insertion-in-a-binary-tree-in-level-order/
+
     def insert(self, val):
         if(self.head == None):
             self.head = Node(val)
@@ -86,21 +105,76 @@ class BinaryTree:
                 q.append(p.right)
         return None, None, None
 
-    @staticmethod
-    def __getVal(root, ans):
-        if(root == None):
-            return
-        BinaryTree.__getVal(root.left, ans)
-        ans.append(root.val)
-        BinaryTree.__getVal(root.right, ans)
-
-    def getVal(self):
+    def bft(self):
+        q = [self.head]
         ans = []
-        BinaryTree.__getVal(self.head, ans)
+        temp = []
+        while q:
+            ans.append([e.val for e in q])
+            while q:
+                p = q.pop(0)
+                if(p.left != None):
+                    temp.append(p.left)
+                if(p.right != None):
+                    temp.append(p.right)
+            q, temp = temp, q
+        return ans
+
+    @staticmethod
+    def __dft_inorder_helper(node, ans):
+        if(node == None):
+            return
+        BinaryTree.__dft_inorder_helper(node.left, ans)
+        ans.append(node.val)
+        BinaryTree.__dft_inorder_helper(node.right, ans)
+
+    def dft_inorder_recursive(self):
+        ans = []
+        BinaryTree.__dft_inorder_helper(self.head, ans)
+        return ans
+
+    # $$$$$$$$$$$$$$$
+    def dft_inorder_iterative(self):
+        # Space - O(1)
+        # Time - O(n) (n=number of nodes)
+        curr = self.head
+        ans = []
+        while curr:
+            if(curr.left != None):
+                pre = curr.left
+                while (pre.right != None) and pre.right != curr:
+                    pre = pre.right
+                if(pre.right == None):
+                    pre.right = curr
+                    curr = curr.left
+                else:
+                    pre.right = None
+                    ans.append(curr.val)
+                    curr = curr.right
+            else:
+                ans.append(curr.val)
+                curr = curr.right
+        return ans
+
+    def dft_preorder_iterative(self):
+        ans = []
+        curr = self.head
+        while curr:
+            ans.append(curr.val)
+            if(curr.left != None):
+                pre = curr.left
+                while pre.right != None and pre.right != curr.right:
+                    pre = pre.right
+                if(pre.right == None):
+                    pre.right = curr.right
+                    curr = curr.left
+                else:
+                    pre.right = None
+                    curr = curr.right
+            else:
+                curr = curr.right
         return ans
 
 
-bt = BinaryTree([1, 2, 3, 4, 5, 6, 7,8])
-print(bt.getVal())
-bt.delete(3)
-print(bt.getVal())
+bt = BinaryTree([1, None, 2])
+print(bt.dft_inorder_recursive())
