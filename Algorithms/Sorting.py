6b896b2ca7e776b574f8431a339add2314a6f7dc
Shubham20091999 <shubham20091999@gmail.com>
2020-09-28 20:01:40 +0530

    Quick Sort

--- Algorithms/Sorting.py
+++ Algorithms/Sorting.py
@@ -1,15 +1,41 @@
-def QuickSort(list,l,r):
-    if(l>=r):
+def PartitionR(list, l, r):
+    v = list[r]
+    i = l
+    for j in range(l, r):
+        if(list[j] < v):
+            list[i], list[j] = list[j], list[i]
+            i += 1
+    list[i], list[r] = list[r], list[i]
+    return i
+
+
+def QuickSortR(list, l, r):
+    if(l >= r):
         return
-    v=list[r]
-    i=l
-    for j in range(l,r):
-        if(list[j]<v):
-            list[i],list[j]=list[j],list[i]
-            i+=1
-    list[i],list[r]=list[r],list[i]
-    QuickSort(list,l,i-1)
-    QuickSort(list,i+1,r)
-a=[9,8,7,6,5,4,3,2,1,0]
-QuickSort(a,0,len(a)-1)
+    i = PartitionR(list, l, r)
+    QuickSortR(list, l, i-1)
+    QuickSortR(list, i+1, r)
+
+
+def PartitionL(list, l, r):
+    X = list[l]
+    i = l
+    for j in range(l+1, r+1):
+        if(list[j] < X):
+            i += 1
+            list[i], list[j] = list[j], list[i]
+    list[l], list[i] = list[i], list[l]
+    return i
+
+
+def QuickSortL(list, l, r):
+    if(l >= r):
+        return
+    i = PartitionL(list, l, r)
+    QuickSortL(list, l, i-1)
+    QuickSortL(list, i+1, r)
+
+
+a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+QuickSortR(a, 0, len(a)-1)
 print(a)
