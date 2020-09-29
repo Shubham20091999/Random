f776d3075338bb1bf063844a4ae1d338a3410f20
Shubham20091999 <shubham20091999@gmail.com>
2020-09-29 11:23:49 +0530

    Reduced Redundancy

--- Algorithms/Sorting.py
+++ Algorithms/Sorting.py
@@ -1,16 +1,8 @@
 from random import randrange
 
-def PartitionRight(list, l, r):
-    v = list[r]
-    i = l
-    for j in range(l, r):
-        if(list[j] < v):
-            list[i], list[j] = list[j], list[i]
-            i += 1
-    list[i], list[r] = list[r], list[i]
-    return i
 
-def PartitionLeft(list, l, r):
+def Partition(list, l, r, ind):
+    list[ind], list[l] = list[l], list[ind]
     v = list[l]
     i = l
     for j in range(l+1, r+1):
@@ -21,10 +13,17 @@ def PartitionLeft(list, l, r):
     return i
 
 
-def PartitionRand(list,l,r):
-    t=randrange(l,r+1)
-    list[t],list[l]=list[l],list[t]
-    return PartitionLeft(list,l,r)
+def PartitionRight(list, l, r):
+    return Partition(list, l, r, r)
+
+
+def PartitionLeft(list, l, r):
+    return Partition(list, l, r, l)
+
+
+def PartitionRand(list, l, r):
+    return Partition(list, l, r, randrange(l, r+1))
+
 
 def QuickSortRight(list, l, r):
     if(l >= r):
@@ -33,6 +32,7 @@ def QuickSortRight(list, l, r):
     QuickSortRight(list, l, i-1)
     QuickSortRight(list, i+1, r)
 
+
 def QuickSortLeft(list, l, r):
     if(l >= r):
         return
@@ -40,12 +40,14 @@ def QuickSortLeft(list, l, r):
     QuickSortLeft(list, l, i-1)
     QuickSortLeft(list, i+1, r)
 
-def QuickSortRand(list,l,r):
-    if(l>=r):
+
+def QuickSortRand(list, l, r):
+    if(l >= r):
         return
-    i=PartitionRand(list,l,r)
+    i = PartitionRand(list, l, r)
     QuickSortRand(list, l, i-1)
-    QuickSortRand(list, i+1, r) 
+    QuickSortRand(list, i+1, r)
+
 
 def kthLargest(list, l, r, k):
     if(l >= r):
@@ -58,8 +60,10 @@ def kthLargest(list, l, r, k):
     else:
         QuickSortLeft(list, l, i-1)
 
-a = [3,14, 17, 13, 15, 19, 10, 3, 16, 9, 12]
-QuickSortRand(a,0,len(a)-1)
+
+a = [3, 14, 17, 13, 15, 19, 10, 3, 16, 9, 12,12,14,6,89,12]
 print(a)
-
-
+QuickSortRight(a, 0, len(a)-1)
+print(a)
+a.sort()
+print(a)
